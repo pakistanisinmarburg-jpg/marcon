43a934343ab477ee18b42f482c5379971d1c8842
pakistanisinmarburg-jpg <pakistanisinmarburg@gmail.com>
2026-05-12 17:45:39 +0200

    Homepage correction

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,13 +1,12 @@
+import HeroSection from '../components/home/HeroSection'
+
 export default function HomePage() {
 
   return (
-    <div
-      style={{
-        padding: '40px',
-        fontSize: '30px'
-      }}
-    >
-      Homepage Working
+    <div>
+
+      <HeroSection />
+
     </div>
   )
 }
